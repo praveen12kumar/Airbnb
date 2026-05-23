edb0aa340ac34f427d1c54c67567408f1d00bece
Praveen Kumar <praveenshakya441@gmail.com>
2026-05-23 12:50:19 +0530

    Convert hotel-service to normal monorepo folder

---
+++ hotel-service/src/config/serverConfig.ts
@@ -0,0 +1,25 @@
+// This files handle the server configuration
+import dotenv from 'dotenv';
+dotenv.config();
+
+type ServerConfig = {
+    PORT: number;
+};
+
+type DBConfig = {
+    DB_HOST: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
+};
+
+export const serverConfig: ServerConfig = {
+    PORT: Number(process.env.PORT) || 3000,
+};
+
+export const dbConfig: DBConfig = {
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || '123@#Pra45',
+    DB_NAME: process.env.DB_NAME || 'airbnb_dev',
+};
